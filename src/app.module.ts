e797ffeee8fb1727e4e3d530abeabab49ebc5cde
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-01-16 11:03:20 +0100

    fix: update AppModule to use App2Service as a provider for AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,6 @@ import { App2Service } from './app2/app2.service';
 @Module({
   imports: [],
   controllers: [AppController, TotoController],
-  providers: [AppService, App2Service],
+  providers: [{ provide: AppService, useClass: App2Service }],
 })
 export class AppModule {}
