f723f70cf6615f67435ee5810672a78f628755ee
Jean-Louis GUENEGO <jlguenego@gmail.com>
2026-01-16 10:53:53 +0100

    feat: add App2Service and its unit tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TotoController } from './toto/toto.controller';
+import { App2Service } from './app2/app2.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TotoController],
-  providers: [AppService],
+  providers: [AppService, App2Service],
 })
 export class AppModule {}
